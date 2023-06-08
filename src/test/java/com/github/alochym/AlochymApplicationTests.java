f90510648b3b97552686bd682783e70801077eb2
alochym <alochym@gmail.com>
2023-06-08 21:53:10 +0700

    add restful starter structure folder

---
+++ src/test/java/com/github/alochym/AlochymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alochym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlochymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
