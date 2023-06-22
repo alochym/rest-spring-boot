d9fa66945f1ec8f13d1c57d4e5e747ea343c970e
alochym <alochym@gmail.com>
2023-06-22 23:23:46 +0700

    finish 404 not found exception

--- src/main/java/com/github/alochym/errors/ErrorExceptionHandler.java
+++ src/main/java/com/github/alochym/errors/ErrorExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpServerErrorException.InternalServerError;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,8 +25,17 @@ public class ErrorExceptionHandler {
         return new ResponseEntity<Object>(notfound, null, notfound.getStatusCode());
     }
 
-    @ExceptionHandler(value = InternalServerError.class)
-    public ResponseEntity<Object> hadnlerServerInternalErrorException(){
-        return new ResponseEntity<Object>(, null, 0)
+    @ExceptionHandler(value = Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ResponseEntity<Object> hadnlerServerInternalErrorException(NotFoundException ex, HttpServletRequest req) {
+
+        CustomError serverError = new CustomError();
+        serverError.setMessage(ex.getMessage());
+        serverError.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+        serverError.addError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        serverError.setReqURL(req.getServletPath());
+
+        return new ResponseEntity<Object>(serverError, null, serverError.getStatusCode());
     }
 }
