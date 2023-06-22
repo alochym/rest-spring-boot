472bc658dbaa462c9cfe9b98f7eb434708cff362
alochym <alochym@gmail.com>
2023-06-22 23:19:50 +0700

    finish 404 not found exception

--- src/main/java/com/github/alochym/errors/ErrorExceptionHandler.java
+++ src/main/java/com/github/alochym/errors/ErrorExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpServerErrorException.InternalServerError;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
@@ -16,8 +18,13 @@ public class ErrorExceptionHandler {
         notfound.setMessage(ex.getMessage());
         notfound.setStatusCode(HttpStatus.NOT_FOUND);
         notfound.addError(HttpStatus.NOT_FOUND.getReasonPhrase());
-        notfound.setReqURL(req.getRequestURI());
+        notfound.setReqURL(req.getServletPath());
 
         return new ResponseEntity<Object>(notfound, null, notfound.getStatusCode());
+    }
+
+    @ExceptionHandler(value = InternalServerError.class)
+    public ResponseEntity<Object> hadnlerServerInternalErrorException(){
+        return new ResponseEntity<Object>(, null, 0)
     }
 }
