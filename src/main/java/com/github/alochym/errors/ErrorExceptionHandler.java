587c1d7def63c4290c5f7f196de912c1df991fac
alochym <alochym@gmail.com>
2023-06-22 23:26:29 +0700

    finish 404 not found exception

--- src/main/java/com/github/alochym/errors/ErrorExceptionHandler.java
+++ src/main/java/com/github/alochym/errors/ErrorExceptionHandler.java
@@ -2,6 +2,7 @@ package com.github.alochym.errors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,15 +26,15 @@ public class ErrorExceptionHandler {
         return new ResponseEntity<Object>(notfound, null, notfound.getStatusCode());
     }
 
-    @ExceptionHandler(value = Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ResponseBody
-    public ResponseEntity<Object> hadnlerServerInternalErrorException(NotFoundException ex, HttpServletRequest req) {
+    public ResponseEntity<Object> hadnlerServerInternalErrorException(HttpServletRequest req) {
 
         CustomError serverError = new CustomError();
-        serverError.setMessage(ex.getMessage());
-        serverError.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-        serverError.addError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        serverError.setMessage(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase());
+        serverError.setStatusCode(HttpStatus.METHOD_NOT_ALLOWED);
+        serverError.addError(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase());
         serverError.setReqURL(req.getServletPath());
 
         return new ResponseEntity<Object>(serverError, null, serverError.getStatusCode());
